d64a700cf155755893c5449126fd856eb345efa0
kcabishek <kcabishek@gmail.com>
2017-03-14 02:09:01 +0000

    Add Facebook like

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,7 @@ router.post("/register", function(req, res){
             return res.redirect("/register");
         }
         passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Welcome to YelpCamp " + user.username);
+           req.flash("success", "Welcome " + user.username);
            res.redirect("/campgrounds"); 
         });
     });
